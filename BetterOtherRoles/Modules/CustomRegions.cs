3d61a0b231de224982adf625c2aa786db108f660
Elouan Martinet <exa@elou.world>
2026-05-23 09:44:26 +0000

    feat: update server list

--- BetterOtherRoles/Modules/CustomRegions.cs
+++ BetterOtherRoles/Modules/CustomRegions.cs
@@ -3,57 +3,19 @@
 public static class CustomRegions
 {
     public static readonly IRegionInfo[] DefaultRegions = {
-        /* Removing Vanilla servers for now as they are not compatible with BOR
-         
-        new StaticHttpRegionInfo("North America",
-            StringNames.ServerNA,
-            "matchmaker.among.us",
-            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
-            {
-                new("Http-1", "https://matchmaker.among.us", 443, false)
-            })).CastFast<IRegionInfo>(),
-        new StaticHttpRegionInfo("Europe",
-            StringNames.ServerEU,
-            "matchmaker-eu.among.us",
-            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
-            {
-                new("Http-1", "https://matchmaker-eu.among.us", 443, false)
-            })).CastFast<IRegionInfo>(),
-        new StaticHttpRegionInfo("Asia",
-            StringNames.ServerAS,
-            "matchmaker-as.among.us",
-            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
-            {
-                new("Http-1", "https://matchmaker-as.among.us", 443, false)
-            })).CastFast<IRegionInfo>(),
-        */
-        new StaticHttpRegionInfo("<color=#DE0D68FF>MODDED [EU]</color>",
-            StringNames.NoTranslation,
-            "https://au-eu.duikbo.at",
-            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
-            {
-                new("Http-1", "https://au-eu.duikbo.at", 443, false)
-            })).CastFast<IRegionInfo>(),
-        new StaticHttpRegionInfo("<color=#2CC71EFF>MODDED [NA]</color>",
-            StringNames.NoTranslation,
-            "www.aumods.xyz",
-            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
-            {
-                new("Http-1", "https://www.aumods.xyz", 443, false)
-            })).CastFast<IRegionInfo>(),
-        new StaticHttpRegionInfo("<color=#C7B816FF>MODDED [AS]</color>",
-            StringNames.NoTranslation,
-            "https://au-as.duikbo.at",
-            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
-            {
-                new("Http-1", "https://au-as.duikbo.at", 443, false)
-            })).CastFast<IRegionInfo>(),
         new StaticHttpRegionInfo("<color=#C7B816FF>BOR [EU]</color>",
             StringNames.NoTranslation,
             "https://amongus-eu.eno.pm",
             new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
             {
                 new("Http-1", "https://amongus-eu.eno.pm", 443, false)
+            })).CastFast<IRegionInfo>(),
+        new StaticHttpRegionInfo("<color=#00C800>Exa [EU]</color>",
+            StringNames.NoTranslation,
+            "https://amongus-eu.eno.pm",
+            new Il2CppReferenceArray<ServerInfo>(new ServerInfo[1]
+            {
+                new("Http-1", "https://impostor.ewd.app", 443, false)
             })).CastFast<IRegionInfo>(),
     };
 }
